0a4325ab456bd24377f38fa2358a446b4b5524d0
ratly <lyj520@gmail.com>
2015-05-19 13:50:30 +0800

    fixed sourcemap

--- src/gulp.js
+++ src/gulp.js
@@ -1,16 +1,40 @@
 var JS_BASEPATH = '';
 var io = require('./io').io,
     _ = require('lodash'),
-    path = require('path');
-
+    path = require('path'),
+    fs = require('fs');
+const MAPS = {
+    'ts'   : ['js',   '.js'],
+    'scss' : ['sass', '.css'],
+    'jpg'  : ['img',  '.jpg'],
+    'png'  : ['img',  '.png'],
+    'gif'  : ['img',  '.gif']
+}
+// 获取被构建的文件
+var _built=function(){try{return require("./.built.json")}catch(e){return[]}}()
+var _built_timer;
+function _getBuildFiles(input) {
+    if(!input) return _built;
+    var ext = path.extname(input);
+    var dest = input.replace(ext, MAPS[ext.slice(1)][1]);
+    var map = dest + '.map';
+    !~_built.indexOf(dest) && _built.push(dest);
+    !~_built.indexOf(map) && _built.push(map);
+    _built_timer = setTimeout(function() {
+        clearTimeout(_built_timer);
+        fs.writeFile(__dirname + '/.built.json', JSON.stringify(_built), 'utf-8'); //debugger
+    }, 1000);
+    return _built;
+}
 function getConfig(staticPath, searchFiles) {
     var config = {
+        root : staticPath,
         ts: {
-            src: path.join(staticPath, '/js/**/*.ts'),
-            dest: path.join(staticPath, '/js/'),
+            src: '**/*.ts',
+            // dest: path.join(staticPath, '/js/'),
             options: {
                 browserify: {
-                    entries:[path.join(staticPath, '/js/gift/main.ts')],
+                    // entries:path.join(staticPath, '/js/gift/main.ts'),
                     extensions: ['.ts'],
                     debug: true
                 },
@@ -23,14 +47,15 @@ function getConfig(staticPath, searchFiles) {
             }
         },
         scss : {
-            src: path.join(staticPath, '/css/**/*.scss'),
-            dest: path.join(staticPath, '/css/'),
+            src: '**/*.scss',
+            config: {
+                outputStyle:'expanded'/*nested, expanded, compact, compressed*/
+            }
         }
     };
 
     var main = io.findMainSync(staticPath, searchFiles);
     _.merge(config, main);
-    // fs.writeFile('./.debuglog.json', JSON.stringify(config, null, 4), 'utf-8');
     return config;
 }
 
@@ -46,9 +71,11 @@ function registerTask(gulp, config) {
         del        = require('del');
 
     gulp.task('ts', function () {
-        config.ts.main.forEach(function(out){
-            out = out.split('/');
-            out = out[out.length - 1].replace('.ts', '.js');
+        config.ts.main.forEach(function(input){
+            var destPath = path.dirname(input);
+            var tmp = input.split('/'), outName = tmp[tmp.length - 1].replace('.ts', '.js');
+            _getBuildFiles(input);
+            config.ts.options.browserify.entries = input;
             browserify(config.ts.options.browserify)
                 .plugin(tsify, config.ts.options.tsify)
                 .bundle()
@@ -58,30 +85,41 @@ function registerTask(gulp, config) {
                     err.end();
                     process.exit(1);
                 })
-                .pipe(source(out))
+                .pipe(source(outName))
                 .pipe(buffer())
                 .pipe(sourcemaps.init({loadMaps: true}))
-                .pipe(sourcemaps.write('./', {includeContent: true, sourceRoot: '/src/scss/'}))
-                .pipe(gulp.dest(config.ts.dest))
+                .pipe(sourcemaps.write('./', {
+                    includeContent: false,
+                    sourceRoot: path.relative(input, config.root)
+                }))
+                // .pipe(gulp.dest(config.ts.dest))
+                .pipe(gulp.dest(destPath))
                 .pipe(notify({ message: 'ts task complete' }));
         });
+
     });
 
     gulp.task('sass', function(){
-        gulp.src(config.scss.main)
-            .pipe(sourcemaps.init())
-            .pipe(sass({outputStyle:'expanded'/*nested, expanded, compact, compressed*/}))
-            .on('error', function(err){
-                notifier.notify({ message: err})
-                sass.logError();
-                // err.end();
-            })
-            .pipe(sourcemaps.write('./', {sourceRoot: config.scss.dest}))
-            .pipe(gulp.dest(config.scss.dest))
+        config.scss.main.forEach(function(input){
+            _getBuildFiles(input);
+            gulp.src(input)
+                .pipe(sourcemaps.init())
+                .pipe(sass(config.scss.config))
+                .on('error', function(err){
+                    notifier.notify({ message: err})
+                    sass.logError();
+                    // err.end();
+                })
+                .pipe(sourcemaps.write('./', {
+                    includeContent: false,
+                    sourceRoot: ''
+                }))
+                .pipe(gulp.dest(path.dirname(input)))
+        });
     });
 
     gulp.task('clear', function(cb) {
-        del('./resource', cb);
+        del(_getBuildFiles(), cb);
     });
 }
 exports.gulp = {
